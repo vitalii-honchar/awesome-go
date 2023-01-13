30741359656975fd13b02203d927292267d8f4db
Vitalii Honchar <weaxme@gmail.com>
2023-01-13 09:56:54 -0500

    Added race condition

---
+++ race-condition/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+
+	var memoryAccess sync.Mutex
+	var data int
+
+	go func() {
+		memoryAccess.Lock()
+		data++
+		memoryAccess.Unlock()
+	}()
+
+	memoryAccess.Lock()
+	if data == 0 {
+		fmt.Println("the value is 0.")
+	} else {
+		fmt.Printf("the value is %v.\n", data)
+	}
+	memoryAccess.Unlock()
+}
